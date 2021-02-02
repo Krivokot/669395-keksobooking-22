b8b14fdfb04ea9935b6525c346c430e6eaf6df6a
Krivokot <37597505+Krivokot@users.noreply.github.com>
2021-02-02 11:57:29 +0300

    Внес исправления
    Исправил названия переменных
    Оптимизировал функцию Int

--- js/main.js
+++ js/main.js
@@ -1,43 +1,6 @@
 'use strict'
 
-const GET_RANDOM = function (start, end) {
-
-  let numbers = [];
-
-  if (start > end) {
-    let swap = start;
-
-    start = end;
-    end = swap;
-  }
-
-  for (let i = start; i <= end; i++) {
-
-    numbers.push(i);
-
-  }
-
-  let rand = numbers[Math.floor(Math.random()*numbers.length)];
-
-  return rand;
-
-};
-
-GET_RANDOM(10, 0);
-
-//или функция из MDN
-
-const GET_RANDOM_MDN = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-GET_RANDOM_MDN(0, 10);
-
-//с плавающей запятой
-
-const GET_RANDOM_GEO = function (start, end, dot) {
+const getRandomFloat = function (start, end, dot = 0) {
 
   if (start > end) {
     let swap = start;
@@ -52,4 +15,9 @@ const GET_RANDOM_GEO = function (start, end, dot) {
 
 };
 
-GET_RANDOM_GEO(0.2, 1, 1);
+
+const getRandomInt = function (start, end) {
+
+  return getRandomFloat(start, end);
+
+};
