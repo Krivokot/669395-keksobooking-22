eb4c82ce2d14875fa9c509dd68e45a462a5ec725
Krivokot <37597505+Krivokot@users.noreply.github.com>
2021-02-04 17:11:47 +0300

    Доработки по третьей домашке
    1. Массивы с данными объектов
    2. Функция генерирующая объекты

--- js/main.js
+++ js/main.js
@@ -22,4 +22,75 @@ const getRandomInt = function (start, end) {
 
 };
 
-getRandomInt(1, 10);
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow'
+];
+
+const CHECKIN_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const CHECKOUT_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const FEAUTERS = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
+
+const ADVS_COUNT = 10;
+
+
+const getRandomArrayElement = function (elements) {
+
+  return elements[getRandomInt(0, elements.length-1)];
+};
+
+const adv = () => {
+  return {
+      author: {
+        avatar: 'img/avatars/user' + '0' + getRandomInt(1,8) + '.png',
+      },
+      location: {
+        x: getRandomFloat(35.65000, 35.70000, 5),
+        y: getRandomFloat(139.70000, 139.80000, 5),
+    },
+      offer: {
+        title: '',
+        address: '',
+        price: getRandomInt(1000, 20000),
+        type: getRandomArrayElement(TYPES),
+        rooms: getRandomInt(1,5),
+        guests: getRandomInt(1,5),
+        checkin: getRandomArrayElement(CHECKIN_TIMES),
+        checkout: getRandomArrayElement(CHECKOUT_TIMES),
+        features: getRandomArrayElement(FEAUTERS),
+        description: '',
+        photos: getRandomArrayElement(PHOTOS),
+      },
+  }
+};
+
+
+const advs = new Array(ADVS_COUNT).fill(null).map(() => adv());
+
+console.log(advs);
